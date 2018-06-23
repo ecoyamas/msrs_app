f7cdc2f89d2e9512403d37095b22ec1b1bf3100a
ecoyamas <hayato53pu@gmail.com>
2018-06-23 20:55:49 +0900

    ユーザー画面今日以降予約表示の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reservation = Reservation.find(:all, :conditions => { :user_id => @user })
+    @day = Date.today
+    @today = @day.to_date
   end
 end
