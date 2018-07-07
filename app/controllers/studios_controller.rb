cf8445242eb85a9795d03e58edf4b0e6d24eb88e
ecoyamas <hayato53pu@gmail.com>
2018-07-07 11:41:21 +0900

    スタジオの登録フォームのレイアウト作成

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,2 +1,17 @@
 class StudiosController < ApplicationController
+  def new
+    @studio = Studio.new
+  end
+
+  def create
+    @studio = Store.new(studios_params)
+    @studio.save
+    redirect_to store_path
+  end
+
 end
+
+private
+  def studios_params
+    params.require(:studio).permit(:studio_name,:store_id)
+  end
