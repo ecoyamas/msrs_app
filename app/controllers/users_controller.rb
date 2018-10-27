dd2e7c274e7266eebb5c1d27f9c41fca7ebfa8a0
ecoyamas <hayato53pu@gmail.com>
2018-10-27 18:40:35 +0900

    マイページに店舗検索機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,22 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reservation = Reservation.where('start_datetime >= ?', Date.today).where(user_id: @user)
+
+    #店舗検索処理
+    @search_store = store_search(params)
   end
 
   def destroy
     User.find(params[:id]).destroy
     redirect_to root_path
+  end
+
+  def store_search(search)
+    if search
+      hoge = Store.where('store_name = ?', params[:search])
+      #検索結果を変数に入れて、変数内を次の条件で検索してまた変数に入れて最後にreturnで値を返す。
+      return hoge
+    end
   end
 
 end
